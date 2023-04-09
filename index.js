a056853ea4e270eb5c21962aba15e8aab0098430
Eugeniu Reveneala <vedsico@gmail.com>
2023-04-09 19:13:58 +0300

    Version 2 - Stable version is now released !

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 $(".input > div:not(.ioperands):not(.iequal)").each(function (index, el) {
     $(this).click(function (e) {
 
-
-        $(".output input").val($(".output input").val() + e.target.innerText)
+        if (!localStorage.getItem("eq")) {
+            $(".output input").val($(".output input").val() + e.target.innerText)
+        }
 
         if (!localStorage.getItem("num1")) {
             localStorage.setItem("num1", e.target.innerText)
@@ -12,24 +13,6 @@ $(".input > div:not(.ioperands):not(.iequal)").each(function (index, el) {
 
         }
 
-        /* if ($.isNumeric(Number(e.target.innerText)) && !localStorage.getItem("num2") && !localStorage.getItem("op")) {
-             if (localStorage.getItem("num1")) {
-             } else {
-
-             }
-             $(".output input").val($(".output input").val() + e.target.innerText)
-             localStorage.setItem("num1", $(".output input").val())
-
-         } else if ((!localStorage.getItem("op") || !$.isNumeric(Number(e.target.innerText))) && (e.target.innerText.indexOf("+") !== -1) || (e.target.innerText.indexOf("-") !== -1)) {
-             $(".output input").val($(".output input").val() + e.target.innerText)
-
-             localStorage.setItem("op", $(".output input").val() + e.target.innerHTMl.slice(-1))
-
-
-         } else if ($.isNumeric(Number(e.target.innerText))) {
-             localStorage.setItem("num2", e.target.innerText)
-
-         }*/
     })
 
 })
@@ -44,9 +27,6 @@ $(".ioperands > div").click(function (e) {
             if (str[i] === "+") indices.push(i);
         }
 
-        console.log(
-            indices
-        )
         if (indices.length <= 1) {
             $(".output input").val($(".output input").val() + "+")
 
@@ -79,7 +59,22 @@ $(".iequal").click(function (e) {
             alert("Provide some numbers!")
     }
 
-    $(".output input").val($(".output input").val() + "=" + eq)
+
+    let ind = function indices(char) {
+        var str = $(".output input").val();
+        var indices = [0];
+        for (var i = 0; i < str.length; i++) {
+            if (str[i] === char) indices.push(i);
+        }
+        return indices
+    }
+
+
+    if ((ind("+").length <= 1 || ind("-").length <= 1) && ind("=").length <= 1) {
+        $(".output input").val($(".output input").val() + "=" + eq)
+
+        localStorage.setItem("eq", eq)
+    }
 })
 
 // clear...
